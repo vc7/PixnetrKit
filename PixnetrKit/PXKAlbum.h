801cfbbaaf646380c8ea8fa516367d0881ec0c1e
Vincent Chen <vince78718@gmail.com>
2014-07-31 17:02:42 +0800

    Add album class and add basic functionalities.

---
+++ PixnetrKit/PXKAlbum.h
@@ -0,0 +1,26 @@
+//
+//  PXKAlbum.h
+//  PixnetrKit
+//
+//  Created by vincent on 2014/07/31.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PXKConstants.h"
+
+@interface PXKAlbum : NSObject
+
++ (void)fetchHotAlbumsWithCategoryIDs:(NSArray *)categoryIds page:(NSUInteger)page perPage:(NSUInteger)perPage resultBlock:(PXKArrayResultBlock)block;
+
+- (id)initWithDictionary:(NSDictionary *)dictionary;
+
+@property (nonatomic) NSInteger objectId;
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *author;
+@property (nonatomic) NSInteger categoryId;
+@property (strong, nonatomic) NSString *categoryName;
+
+- (NSString *)previewImageURLString;
+
+@end
---
+++ PixnetrKit/PXKAlbum.m
@@ -0,0 +1,56 @@
+//
+//  PXKAlbum.m
+//  PixnetrKit
+//
+//  Created by vincent on 2014/07/31.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <PIXNETSDK.h>
+
+#import "PXKAlbum.h"
+#import "PXKURLGenerator.h"
+
+@implementation PXKAlbum
+
+@synthesize previewImageURLString = _previewImageURLString;
+
++ (void)fetchHotAlbumsWithCategoryIDs:(NSArray *)categoryIds page:(NSUInteger)page perPage:(NSUInteger)perPage resultBlock:(PXKArrayResultBlock)block
+{
+    [[PIXNETSDK sharedInstance] getMainpageAlbumsWithCategoryIDs:@[@"25"] albumType:PIXMainpageTypeHot page:page perPage:perPage strictFilter:1 completion:^(BOOL succeed, id result, NSError *error) {
+        
+        if (succeed) {
+            __block NSMutableArray *albumArray = [NSMutableArray array];
+            
+            [[result objectForKey:@"sets"] enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+                [albumArray addObject:[[PXKAlbum alloc] initWithDictionary:obj]];
+            }];
+            
+            block(albumArray, nil);
+            
+        } else {
+            block(nil, error);
+        }
+    }];
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super init];
+    if (self) {
+        self.objectId = [[dictionary objectForKey:@"id"] integerValue];
+        self.title = [dictionary objectForKey:@"title"];
+        self.categoryId = [[dictionary objectForKey:@"category_id"] integerValue];
+        self.categoryName = [dictionary objectForKey:@"category"];
+    }
+    return self;
+}
+
+#pragma mark - Accessor
+
+- (NSString *)previewImageURLString
+{
+    return [PXKURLGenerator generateAlbumPreviewImageURLStringWithAlbum:self size:(CGSize){ 20, 50 }];
+}
+
+@end
