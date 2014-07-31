a4d33b725602f7a221f2a6d33c51a54251497307
Vincent Chen <vince78718@gmail.com>
2014-07-31 17:03:10 +0800

    Add Photo model class based on Parse object.

---
+++ PixnetrKit/PXKPhoto.h
@@ -0,0 +1,15 @@
+//
+//  PXKPhoto.h
+//  PixnetrKit
+//
+//  Created by vincent on 2014/07/31.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <Parse.h>
+
+@interface PXKPhoto : PFObject <PFSubclassing>
+
++ (NSString *)parseClassName;
+
+@end
---
+++ PixnetrKit/PXKPhoto.m
@@ -0,0 +1,20 @@
+//
+//  PXKPhoto.m
+//  PixnetrKit
+//
+//  Created by vincent on 2014/07/31.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <PFObject+Subclass.h>
+
+#import "PXKPhoto.h"
+
+@implementation PXKPhoto
+
++ (NSString *)parseClassName
+{
+    return @"Photo";
+}
+
+@end
