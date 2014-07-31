13628872a822a769dffe00aea83208ad3f8eba66
Vincent Chen <vince78718@gmail.com>
2014-07-31 14:24:31 +0800

    Add constants file to store universal usage things.

---
+++ PixnetrKit/PXKConstants.h
@@ -0,0 +1,13 @@
+//
+//  PXKConstants.h
+//  PixnetrKit
+//
+//  Created by vincent on 2014/07/31.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class PXKUser;
+
+typedef void (^PXKUserResultBlock)(PXKUser *user, NSError *error);
---
+++ PixnetrKit/PXKConstants.m
@@ -0,0 +1,9 @@
+//
+//  PXKConstants.m
+//  PixnetrKit
+//
+//  Created by vincent on 2014/07/31.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import "PXKConstants.h"
